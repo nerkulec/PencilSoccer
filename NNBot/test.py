076baa85c037a272e57a50d63569f026526a1ba1
Bartosz Brzoza <bartoszbrzoza1@gmail.com>
2018-09-04 21:28:55 +0200

    Better training

--- NNBot/test.py
+++ NNBot/test.py
@@ -2,7 +2,7 @@ from PencilGame.pencilGame import Game
 from PencilGame.bots import RandomBot, NNBot, SearchBot
 from keras.models import load_model
 
-game = Game(value_model=load_model('saves/pencilbot_v2.h5'))
-# game.set_bot_P1()
-game.set_bot_P2(NNBot(game, 'v2'))
+game = Game(value_model=load_model('saves/pencilbot_v2.1.h5'))
+# game.set_bot_P1(NNBot(game, 'v2.1'))
+game.set_bot_P2(NNBot(game, 'v2.1'))
 game.play()
--- NNBot/train.py
+++ NNBot/train.py
@@ -9,19 +9,19 @@ import random
 from tqdm import tqdm
 import numpy as np
 
-epochs = 10
-games_in_epoch = 100
+epochs = 1000
+games_in_epoch = 10
 batch_size = 10
-discount_ratio = 0.7
-# searchbot_chance = 1
-explore = 0.8
+discount_ratio = 0.9
+searchbot_chance = 0.7
+explore = 0
 test_games = 1000
 
-version = 'v2'
+version = 'v2.1'
 
 V = Sequential()
-V.add(Conv2D(16, (3, 3), input_shape=(9, 8, 7), activation='relu', padding="valid"))
-V.add(Conv2D(32, (3, 3), activation='relu', padding="valid"))
+V.add(Conv2D(32, (3, 3), input_shape=(9, 8, 7), activation='relu', padding="valid"))
+V.add(Conv2D(64, (3, 3), activation='relu', padding="valid"))
 V.add(Flatten())
 V.add(Dense(256, activation='relu'))
 V.add(Dense(128, activation='relu'))
@@ -29,13 +29,14 @@ V.add(Dense(64, activation='relu'))
 V.add(Dense(8, activation='relu'))
 V.add(Dense(1, activation='tanh'))
 
-V.compile(optimizer=Adam(lr=0.1), loss='mse')
+V.compile(optimizer=Adam(lr=0.001), loss='mse')
 
 for epoch in tqdm(range(epochs)):
     epoch_envs = []
     epoch_rewards = []
     for game_num in range(games_in_epoch):
         game = Game()
+        searchbot = SearchBot(game, 3)
         envs = []
         rewards = []
         while True:
@@ -43,23 +44,32 @@ for epoch in tqdm(range(epochs)):
                 _, _, winner, _ = game.step(random.randint(0, 7))
                 env = game.get_env()
                 envs.append(env)
-                rewards.append(V.predict(np.array([env]))[0][0])
+                if winner:
+                    rewards.append(1 if winner is "P1" else -1)
+                else:
+                    rewards.append(0)
+            elif random.random() < searchbot_chance:
+                _, _, winner, _ = game.step(searchbot.get_move())
+                env = game.get_env()
+                envs.append(env)
+                if winner:
+                    rewards.append(1 if winner is "P1" else -1)
+                else:
+                    rewards.append(0)
             else:
                 values = []
                 for move_num in range(8):
                     _, _, winner, overwriting = game.step(move_num)
-                    if winner:
-                        values.append(1 if winner is "P1" else -1)
-                    else:
-                        values.append(V.predict(np.array([game.get_env()]))[0][0])
+                    values.append(V.predict(np.array([game.get_env()]))[0][0])
                     game.undo_last_step(overwriting)
-                # print(np.array(values))
-                # print()
                 best_value = max(values) if game.turn is "P1" else min(values)
                 best_move_num = np.argmax(values) if game.turn is "P1" else np.argmin(values)
                 _, _, winner, _ = game.step(best_move_num)
                 envs.append(game.get_env())
-                rewards.append(best_value)
+                if winner:
+                    rewards.append(1 if winner is "P1" else -1)
+                else:
+                    rewards.append(0)
             if winner:
                 break
         for i in reversed(range(len(rewards)-1)):
@@ -69,7 +79,6 @@ for epoch in tqdm(range(epochs)):
         epoch_rewards.extend(rewards)
     epoch_envs = np.array(epoch_envs)
     epoch_rewards = np.array(epoch_rewards)
-    V.fit(epoch_envs, epoch_rewards, initial_epoch=epoch, callbacks=[TensorBoard(log_dir='./logs')])
+    V.train_on_batch(epoch_envs, epoch_rewards)
 
-    # V.fit
 V.save('saves/pencilbot_{}.h5'.format(version))
